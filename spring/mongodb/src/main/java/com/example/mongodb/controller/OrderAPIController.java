df6da98770a07021aa44b34538b4972d61bf764c
hoanghiep8921 <hoanghiep8921@gmail.com>
2020-06-04 18:56:04 +0700

    update

--- spring/mongodb/src/main/java/com/example/mongodb/controller/OrderAPIController.java
+++ spring/mongodb/src/main/java/com/example/mongodb/controller/OrderAPIController.java
@@ -25,128 +25,190 @@ public class OrderAPIController {
     @Autowired
     OrderRepository orderRepository;
 
-    //lấy sản phẩm hiện tại trong giỏ hàng của user
+    //lấy sản phẩm hiện tại trong giỏ hàng của user theo id
+    //Nếu không có tự động thêm mới giỏ hàng trống
     @RequestMapping("/products/{id}")
     public BaseResponse getListProductInCart(
             @PathVariable("id")String id){
+        //Order có trạng thái = 1 chính là giỏ hàng hiện tại của user
         BaseResponse response = new BaseResponse();
         Optional<Order> optionalOrder = orderRepository.findByBuyerAndStatus(id,1);
-        //1,New account
-        //2,Just ordered
+        //Không tồn tại order nào có trạng thái = 1 tức là :
+        //1,New account : Là tài khoản mới đc tạo chưa có giỏ hàng
+        //2,Just ordered : Hóa đơn mới đc chuyển từ 1 -> 2.
+        // Không còn hóa đơn nào ở trạng thái là 1 -> tức là giỏ hàng
         if(!optionalOrder.isPresent()){
+            //Thực hiện tạo mới
+
             Order order = new Order();
             order.setBuyer(id);
             //order.setId("MH235");
             order.setStatus(1);
             orderRepository.save(order);
+
             response.setCode("00");
             response.setMessage("Create new cart for user " + id);
             response.setData(order);
         }else{
             Order exits = optionalOrder.get();
             response.setCode("00");
-            response.setMessage("Create new cart for user " + id);
+            response.setMessage("Get product in cart for user " + id);
             response.setData(exits);
         }
         return response;
     }
 
+    //cập nhập lại giỏ hàng ( thêm, xóa, sửa ) theo id của người dùng
     @RequestMapping(value = "/update/{id}",method = RequestMethod.PUT)
     public BaseResponse updateCart(
+            //map với {id} ở trên requestMapping
             @PathVariable("id") String id,
             @RequestBody UpdateCartRequest updateCartRequest){
         BaseResponse response = new BaseResponse();
+        //Tìm kiếm giỏ hàng trong DB
         Optional<Order> optOrder = orderRepository.findById(id);
+        //nếu không tồn tại
         if(!optOrder.isPresent()){
             response.setData(null);
             response.setMessage("Data not found");
             response.setCode("99");
             return response;
         }
+        //tồn tài thì lấy ra thông tin giỏ hàng
         Order exitsOrder = optOrder.get();
+        //Trạng thái hóa đơn khác 1 tức là k phải giỏ hàng
+        //thông báo lỗi
         if(exitsOrder.getStatus() != 1){
             response.setData(null);
             response.setMessage("Cart invalid");
             response.setCode("98");
             return response;
         }
-        //checkname
+        //Có thể check name của order có giống với
+        //name của user gửi trong UpdateRequest
+        //nếu khác không cho update
+
+        //Code here
+
+        //Lấy danh sách sản phẩm mà user muốn thêm
         List<ProductCart> lstProduct =
                 updateCartRequest.getLstProductCart();
 
+        //Lấy danh sách sản phẩm trong giỏ hàng hiện tại
         List<ProductModel> productInCart = exitsOrder.getListProduct();
+
+        //Duyệt danh sách sản phẩm người dùng gửi lên
         for(ProductCart p : lstProduct){
+            //kiểm tra type = 1 : là thêm || 2 là xóa
             //add product
             if(p.getType() == 1){
+                //Sản phẩm trong giỏ hàng hiện tại trong DB không có
                 if (productInCart == null) {
+                    //Tạo mới giổ hàng trống
                     productInCart = new ArrayList<>();
+                    //Tạo đối tượng sản phẩm trong giỏ hàng với các thông tin sau
                     ProductModel productModel = new ProductModel();
+                    //số lượng client gửi lên
                     productModel.setNumber(p.getNumber());
+                    //Mã sp client gửi lên
                     productModel.setId(p.getId());
 
                     //lấy thông tin sản phẩm trong DB
                     Product exitsProduct = productRepository.findById(p.getId()).get();
+                    //Thêm giá
                     productModel.setPrice(exitsProduct.getPrice());
+                    //Thêm name
                     productModel.setName(exitsProduct.getName());
+                    //Thêm ảnh
                     productModel.setImage(exitsProduct.getImage());
 
+                    //Thêm sản phẩm vưa tạo vào Danh sách sẩn phẩm trong giỏ
                     productInCart.add(productModel);
+
                     response.setCode("00");
                     response.setMessage("Thêm sản phẩm vào giỏ thành công");
                 }else{
-                    for(ProductModel pm : productInCart){
-                        //p.getNumber là số sản phẩm muốn thêm
-                        //check số sản phẩm hiện tại trong DB phải lớn hơn number
+                    boolean isHaveInList = false;
+                    //Duyệt sản phẩm trong danh sách sp trong giỏ hàng của DB đang có sẵn
+                    for(int i=0; i < productInCart.size();i++){
+                        ProductModel pm = productInCart.get(i);
+                        //p.getNumber là số sản phẩm người dùng muốn thêm
+                        //pm.getNumber là số sản phẩm hiện tại trong gio hàng của DB
+                        //so sánh mã số sản phẩm hiện tại trong DB và mã người dùng
+                        //gửi lên nếu = nhau tăng số lượng lên
                         if(p.getId().equals(pm.getId()) && p.getNumber() > 0){
+                            //Đặt lại số lượng của sản phẩm trong giỏ hàng
                             pm.setNumber(pm.getNumber() + p.getNumber());
+                            isHaveInList = true;
                             response.setCode("00");
                             response.setMessage("Đã tăng số lượng thành công");
-                        }else{
-                            //Nếu không có trong list product của cart thì thêm mới
-                            //product vào trong cart
-                            ProductModel productModel = new ProductModel();
-                            productModel.setNumber(pm.getNumber());
-                            productModel.setId(pm.getId());
-                            //lấy thông tin sản phẩm trong DB
-                            Product exitsProduct = productRepository.findById(pm.getId()).get();
-                            productModel.setPrice(exitsProduct.getPrice());
-                            productModel.setImage(exitsProduct.getImage());
-
-                            productInCart.add(pm);
-                            response.setCode("00");
-                            response.setMessage("Thêm sản phẩm vào giỏ thành công");
                         }
                     }
-                }
 
+                    if(!isHaveInList){
+                        //Nếu sản phẩm gửi lên không có trong list product của
+                        // cart thì thêm mới sản phẩm đó vào trong ds sản phẩm của
+                        // Giỏ hàng
+                        //Tạo mới model và set các trường
+                        ProductModel productModel = new ProductModel();
+                        //Số lượng
+                        productModel.setNumber(p.getNumber());
+                        productModel.setId(p.getId());
+                        //lấy thông tin sản phẩm trong DB
+                        Product exitsProduct = productRepository.findById(p.getId()).get();
+                        productModel.setPrice(exitsProduct.getPrice());
+                        productModel.setName(exitsProduct.getName());
+                        productModel.setImage(exitsProduct.getImage());
+
+                        //lưu lại sản phẩm vào giỏ hàng
+                        productInCart.add(productModel);
+
+                        response.setCode("00");
+                        response.setMessage("Thêm sản phẩm vào giỏ thành công");
+
+                    }
+                }
             }
+            //type = 2 là xóa
             //delete
             if(p.getType() == 2){
-                for(ProductModel pm : productInCart){
-                    //p.getNumber là số sản phẩm muốn thêm
-                    //check số sản phẩm hiện tại trong DB phải lớn hơn number
-                    if(p.getId().equals(pm.getId()) &&
-                            p.getNumber() > 0 &&
-                            p.getNumber() <= pm.getNumber() ){
+                boolean isHaveInList = false;
+                for(int i = 0;i < productInCart.size();i++){
+                    ProductModel pm =productInCart.get(i);
+                    //p.getNumber là số lượng sản phẩm muốn thêm
+                    //pm.getNumber là số lượng sản phẩm hiện tại
+                    //so sánh mã số sản phẩm hiện tại trong DB
+                    //và mã sp người dùng gửi lên
+                    //nếu trùng thực hiện giảm số lượng sản phẩm
+                    if(p.getId().equals(pm.getId()) && p.getNumber() > 0 &&
+                            p.getNumber() <= pm.getNumber()){
+
+                        //Giẩm số lượng sản phẩm
                         pm.setNumber(pm.getNumber() - p.getNumber());
-                        //nếu giảm xuống == 0 thì xóa luôn sản phẩm khỏi list
                         response.setMessage("Đã giảm số lượng sản phẩm");
+
+                        //Nếu giảm xuống == 0 thì xóa luôn sản phẩm khỏi list
                         if(pm.getNumber() == 0){
                             productInCart.remove(pm);
                             response.setMessage("Đã xóa sản phẩm ");
                         }
+
                         response.setCode("00");
-                    }else{
-                        response.setCode("99");
-                        response.setMessage("Data invalid");
                     }
+                }
+                if(!isHaveInList){
+                    response.setCode("99");
+                    response.setMessage("Data invalid");
                 }
             }
         }
+
+        //Lưu lại danh sách sản phẩm vào giở hàng của người dùng
         exitsOrder.setListProduct(productInCart);
+        //Lưu lại giỏ hàng vào Database
+        response.setData(orderRepository.save(exitsOrder));
         response.setCode("00");
         response.setMessage("Update giỏ hàng thành công");
-        response.setData(orderRepository.save(exitsOrder));
         return response;
     }
 
--- spring/mongodb/src/main/java/com/example/mongodb/services/OrderServices.java
+++ spring/mongodb/src/main/java/com/example/mongodb/services/OrderServices.java
@@ -1,7 +1,37 @@
 package com.example.mongodb.services;
 
+import com.example.mongodb.model.Order;
+import com.example.mongodb.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrderServices {
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    //tìm kiếm product theo name và giá tiền
+    public List<Product> search(String name, long price, Pageable pageable){
+        Query query = new Query();
+        //check name tồn tài mới thêm điều kiện search
+        if(!name.isEmpty()){
+            query.addCriteria(Criteria.where("name").regex(name));
+        }
+        //check giá lớn hơn 0  mới thêm điều kiện search
+        if(price > 0){
+            query.addCriteria(Criteria.where("price").lte(price));
+        }
+        //nếu khác null là phân trang và sắp xếp theo pageanable
+        if(pageable != null){
+            query.with(pageable);
+        }
+        List<Product> lstProduct = mongoTemplate.find(query, Product.class);
+        return lstProduct;
+    }
 }
