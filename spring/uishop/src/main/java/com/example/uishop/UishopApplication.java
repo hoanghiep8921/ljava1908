33d57f58b2856938d07d6f2a7c87d11c7e056579
hoanghiep8921 <hoanghiep8921@gmail.com>
2020-06-11 21:09:31 +0700

    update

---
+++ spring/uishop/src/main/java/com/example/uishop/UishopApplication.java
@@ -0,0 +1,13 @@
+package com.example.uishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UishopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UishopApplication.class, args);
+    }
+
+}
---
+++ spring/uishop/src/test/java/com/example/uishop/UishopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UishopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
